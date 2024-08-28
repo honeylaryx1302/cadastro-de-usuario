8f22314bec055a86e090e48e05403a666dc298d7
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-08-28 09:08:15 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,25 @@
+class Calculadora:
+    def adicionar(self, a, b):
+        return a + b
+
+    def subtrair(self, a, b):
+        return a - b
+
+    def multiplicar(self, a, b):
+        return a * b
+
+    def dividir(self, a, b):
+        if b == 0:
+            return "Erro: Divisão por zero não é permitida"
+        return a / b
+    
+    def restoDaDivisao(self, a, b):
+        return a % b
+
+c = Calculadora()
+
+print("adicionar: ", c.adicionar(10,5))
+print("subtrair: ", c.subtrair(10,5))
+print("multiplicar: ", c.multiplicar(10,5))
+print("dividir: ", c.dividir(10,5))
+print("restoDaDivisao: ", c.restoDaDivisao(10,5))
